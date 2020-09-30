c9cf9b541db92c67d03e1a13a1010927351d4a8d
Thomas Bollmeier <developer@thomas-bollmeier.de>
2020-09-30 07:48:11 +0200

    Fix GHappy class

--- src/test/java/tudelft/ghappy/GHappyTest.java
+++ src/test/java/tudelft/ghappy/GHappyTest.java
@@ -1,4 +1,33 @@
 package tudelft.ghappy;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class GHappyTest {
+
+    private GHappy cut;
+
+    @BeforeEach
+    public void setUp() {
+        cut = new GHappy();
+    }
+
+    @Test
+    public void testGHappy() {
+
+        Assertions.assertTrue(cut.gHappy("xxggxx"));
+        Assertions.assertFalse(cut.gHappy("xxgxx"));
+        Assertions.assertFalse(cut.gHappy("xxggyygxx"));
+
+    }
+
+    @Test
+    public void startsWithG() {
+
+        Assertions.assertTrue(cut.gHappy("ggxx"));
+        Assertions.assertFalse(cut.gHappy("gxx"));
+
+    }
+
 }
